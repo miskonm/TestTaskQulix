16db841ab95be43ea0bcf7e2b82b40b9bddc7640
miskonm <miskonm@gmail.com>
2016-12-08 18:39:29 +0300

    Initial Commit

---
+++ TestTaskQulix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestTaskQulix
+//
+//  Created by Nikita Misko on 08.12.16.
+//  Copyright © 2016 Nikita Misko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
